97293fad8563a340545522fc252b4285bde5bd0a
Neroo <e.z.millan07@gmail.com>
2022-08-09 20:45:19 -0500

    Ping pong

---
+++ main.py
@@ -0,0 +1,47 @@
+from turtle import Screen
+from player_paddle import Paddle
+from ball import Ball
+from score import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+score = Scoreboard()
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+
+
+screen.listen()
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+
+
+game_on = True
+while game_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move_ball()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with right paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        score.l_point()
+        speed = 0.1
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        score.r_point()
+        speed = 0.1
+screen.exitonclick()
